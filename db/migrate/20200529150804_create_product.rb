051e57b878b9f0262526913ec7f5ec5f3069166f
jhell85 <jhell85@gmail.com>
2020-05-29 11:04:48 -0700

    can't get database to register with app

--- db/migrate/20200529150804_create_product.rb
+++ db/migrate/20200529150804_create_product.rb
@@ -1,9 +1,11 @@
 class CreateProduct < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.string :name :string
+      t.string :name 
       t.integer :cost
       t.sting :country_of_orgin
+
+      t.timestamps
     end
   end
 end
